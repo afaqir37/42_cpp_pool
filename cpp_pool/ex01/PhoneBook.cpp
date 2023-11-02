d37585ed599b2410c1954191041651cbdd462b12
Faqir Abdellah <111251827+afaqir37@users.noreply.github.com>
2023-11-02 02:48:51 +0000

    ex01: in_progress

--- cpp_pool/ex01/PhoneBook.cpp
+++ cpp_pool/ex01/PhoneBook.cpp
@@ -1,13 +1,44 @@
 #include "PhoneBook.hpp"
 
+void    PhoneBook::add(void)
+{
+    if (index == 8)
+        index = 0;
+    book[index].init();
+    index++;
+}
+
+void    PhoneBook::search(void)
+{
+    int tmp;
+
+    std::cout << "Enter an index: ";
+    std::cin >> tmp;
+    book[tmp].display();
+
+}
 int main(void)
 {
+    PhoneBook   phonebook;
     std::string command;
+    
 
-    while (command.compare("ADD") && command.compare("SEARCH") && command.compare("EXIT"))
+    phonebook.index = 0;
+    while (1)
     {
         std::cout << "Enter a command: ";
-        std::cin >> command;
+        std::getline(std::cin, command);
+        if (command.compare("ADD") && command.compare("SEARCH") && command.compare("EXIT"))
+            continue;
+        
+    
+    if (!command.compare("ADD"))
+        phonebook.add();
+    if (!command.compare("SEARCH"))
+        phonebook.search();
+    if (!command.compare("EXIT"))
+        return 0;
+
     }
 
     
--- cpp_pool/ex01/PhoneBook.hpp
+++ cpp_pool/ex01/PhoneBook.hpp
@@ -1,21 +1,50 @@
 #ifndef _PHONE_BOOK_
-# define _PHONE_BOOK_
+#define _PHONE_BOOK_
 #include <iostream>
 #include <string>
 
-class Contact {
-public:
+class Contact
+{
+private:
     std::string firstName;
     std::string lastName;
     std::string nickname;
     std::string phoneNumber;
     std::string darkestSecret;
+
+public:
+    void init(void)
+    {
+        std::cout << "Enter first name: ";
+        std::getline(std::cin, firstName);
+        std::cout << "Enter last name: ";
+        std::getline(std::cin, lastName);
+        std::cout << "Enter a nickname: ";
+        std::getline(std::cin, nickname);
+        std::cout << "Enter the phone number: ";
+        std::getline(std::cin, phoneNumber);
+        std::cout << "Enter the darkestSecret: ";
+        std::getline(std::cin, darkestSecret);
+    }
+
+    void display(void)
+    {
+        std::cout << firstName << std::endl;
+        std::cout << lastName << std::endl;
+        std::cout << nickname << std::endl;
+        std::cout << phoneNumber << std::endl;
+        std::cout << darkestSecret << std::endl;
+    }
 };
 
-
-class PhoneBook {
+class PhoneBook
+{
     Contact book[8];
+
+public:
+    int index;
+    void add(void);
+    void search(void);
 };
 
-
-# endif
+#endif
