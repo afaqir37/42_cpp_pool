20d540ca235c5c4799ba6170831092d5723ea60e
Faqir Abdellah <111251827+afaqir37@users.noreply.github.com>
2024-03-27 02:26:27 +0000

    day07/ex01 done and update README.md

--- cpp_pool/day07/ex01/iter.hpp
+++ cpp_pool/day07/ex01/iter.hpp
@@ -3,6 +3,16 @@
 
 #include <iostream>
 
+template<typename T, typename Func>
+void iter(T* arr, size_t length, Func func) {
+    for (size_t i = 0; i < length; i++) {
+        func(arr[i]);
+    }
+}
 
+template<typename T>
+void printElem(const T& x) {
+    std::cout << x << std::endl;
+}
 
 #endif
--- cpp_pool/day07/ex01/main.cpp
+++ cpp_pool/day07/ex01/main.cpp
@@ -1,1 +1,9 @@
 #include "iter.hpp"
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    iter(arr, 5, printElem<int>);
+
+    int array[] = {'a', 'b', 'c', 'd', 'e'};
+    iter(array, 5, printElem<char>);
+}
